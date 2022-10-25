1049c72d96ad61779cfcd3ab5ab23aca7253ea02
Iliya Draganov <iliya.draganov@mentormate.com>
2022-10-25 20:15:38 +0300

    generic seed functions started

--- src/lib/database/helpers.rs
+++ src/lib/database/helpers.rs
@@ -1,5 +1,5 @@
-use super::errors::DatabaseError;
-use log::info;
+use super::errors::{DatabaseError, SeedDatabaseError};
+use log::{error, info};
 use serde::{Deserialize, Serialize};
 use std::io::Write;
 use std::{
@@ -16,14 +16,21 @@ pub trait HasConfig {
     fn get_config<'a>(&'a self) -> &'a Option<serde_json::Value>;
 }
 
-pub fn is_data_secure<Secure, Candidate, ConfigType>(
+pub trait Predefined<'a, Model>
+where
+    Model: Serialize + Deserialize<'a>,
+{
+    fn get_predefined() -> Vec<Model>;
+}
+
+pub fn is_data_secure<'a, Secure, Candidate, ConfigType>(
     consts: &Vec<Secure>,
     candidates: &Vec<Candidate>,
 ) -> Result<bool, DatabaseError>
 where
-    Secure: Debug + HasName + HasConfig,
-    Candidate: Debug + HasName + HasConfig,
-    ConfigType: Serialize + for<'a> Deserialize<'a>,
+    Secure: Debug + HasName + HasConfig + Serialize + Deserialize<'a>,
+    Candidate: Debug + HasName + HasConfig + Serialize + Deserialize<'a>,
+    ConfigType: Debug + Serialize + Deserialize<'a>,
 {
     for secure in consts.iter() {
         let found = candidates
@@ -39,7 +46,15 @@ where
     Ok(true)
 }
 
-pub fn seed_file_check<T: Serialize + for<'a> Deserialize<'a>>(path: &String, predefined: Vec<T>) {
+pub fn seed_file_check<'a, PredefinedSeed, CandidateSeed, SeedConfig>(
+    path: &String,
+    predefined: Vec<PredefinedSeed>,
+) -> Result<(), SeedDatabaseError>
+where
+    PredefinedSeed: Debug + HasName + HasConfig + Serialize + Deserialize<'a>,
+    CandidateSeed: Debug + HasName + HasConfig + Serialize + Deserialize<'a>,
+    SeedConfig: Debug + Serialize + Deserialize<'a>,
+{
     let file_path = Path::new(&path);
     let json_file_contents = match fs::read_to_string(file_path).ok() {
         Some(contents) => contents,
@@ -49,7 +64,7 @@ pub fn seed_file_check<T: Serialize + for<'a> Deserialize<'a>>(path: &String, pr
         }
     };
 
-    let seeds = match serde_json::from_str::<Vec<T>>(&json_file_contents).ok() {
+    let seeds = match serde_json::from_str::<Vec<CandidateSeed>>(&json_file_contents).ok() {
         Some(res) => res,
         None => {
             info!("JSON array in {} is invalid! Recovering...", path);
@@ -64,7 +79,16 @@ pub fn seed_file_check<T: Serialize + for<'a> Deserialize<'a>>(path: &String, pr
         path
     );
 
-    if seeds.len() < predefined.len() {
+    let secure =
+        match is_data_secure::<PredefinedSeed, CandidateSeed, SeedConfig>(&predefined, &seeds) {
+            Ok(result) => result,
+            Err(err) => {
+                error!("{}", err);
+                return Err(SeedDatabaseError::SeedCorruptionAttempt);
+            }
+        };
+
+    if seeds.len() < predefined.len() || secure == false {
         info!("Overwriting {}", path);
         let mut file = OpenOptions::new()
             .write(true)
@@ -74,4 +98,10 @@ pub fn seed_file_check<T: Serialize + for<'a> Deserialize<'a>>(path: &String, pr
         let text = serde_json::to_string(&predefined).unwrap();
         writeln!(file, "{}", text).expect(&format!("Failed to write to file {}", &path));
     }
+
+    Ok(())
+}
+
+pub fn get_max_level() -> u32 {
+    u32::MAX
 }
--- src/lib/database/models/role_group/mod.rs
+++ src/lib/database/models/role_group/mod.rs
@@ -7,7 +7,9 @@ use serde::{Deserialize, Serialize};
 use serde_json::json;
 use uuid::Uuid;
 
-use crate::database::helpers::{is_data_secure, seed_file_check, HasConfig, HasName};
+use crate::database::helpers::{
+    get_max_level, is_data_secure, seed_file_check, HasConfig, HasName, Predefined,
+};
 use crate::database::{
     errors::{DatabaseError, SeedDatabaseError},
     schema::role_groups,
@@ -27,16 +29,20 @@ pub struct RoleGroup {
 }
 
 const SYSTEM_ROLE_GROUP_NAME: &str = "SYSTEM";
-// const ADMIN_ROLE_GROUP_NAME: &str = "ADMIN";
-// const CLIENT_ROLE_GROUP_NAME: &str = "CLIENT";
-// const USER_ROLE_GROUP_NAME: &str = "USER";
+const ADMIN_ROLE_GROUP_NAME: &str = "ADMIN";
+const CLIENT_ROLE_GROUP_NAME: &str = "CLIENT";
+const USER_ROLE_GROUP_NAME: &str = "USER";
+
+const ADMIN_ROLE_LEVEL: u32 = 100_000;
+const CLIENT_ROLE_LEVEL: u32 = 10_000;
+const USER_ROLE_LEVEL: u32 = 1_000;
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct RoleGroupConfig {
     level: Option<u32>,
 }
 
-#[derive(Serialize, Deserialize, Insertable, Debug)]
+#[derive(Debug, Serialize, Deserialize, Insertable)]
 #[diesel(table_name = role_groups)]
 pub struct RoleGroupForm {
     name: String,
@@ -65,6 +71,29 @@ impl HasConfig for RoleGroupForm {
     }
 }
 
+impl Predefined<'_, RoleGroupForm> for RoleGroupForm {
+    fn get_predefined() -> Vec<RoleGroupForm> {
+        vec![
+            RoleGroupForm {
+                name: SYSTEM_ROLE_GROUP_NAME.to_string(),
+                config: Some(json!({ "level": get_max_level() })),
+            },
+            RoleGroupForm {
+                name: ADMIN_ROLE_GROUP_NAME.to_string(),
+                config: Some(json!({ "level": ADMIN_ROLE_LEVEL })),
+            },
+            RoleGroupForm {
+                name: CLIENT_ROLE_GROUP_NAME.to_string(),
+                config: Some(json!({ "level": CLIENT_ROLE_LEVEL })),
+            },
+            RoleGroupForm {
+                name: USER_ROLE_GROUP_NAME.to_string(),
+                config: Some(json!({ "level": USER_ROLE_LEVEL })),
+            },
+        ]
+    }
+}
+
 impl RoleGroup {
     pub fn get_all_role_groups(
         connection: &mut PgConnection,
@@ -80,29 +109,29 @@ impl RoleGroup {
         Ok(seeded_role_groups)
     }
 
-    pub fn insert(
-        connection: &mut PgConnection,
-        role_groups: &Vec<RoleGroup>,
-    ) -> Result<Vec<RoleGroup>, DatabaseError> {
-        let untouchables: Vec<RoleGroupForm> = RoleGroup::get_predefined();
-        if let Err(err) =
-            is_data_secure::<RoleGroupForm, RoleGroup, RoleGroupConfig>(&untouchables, role_groups)
-        {
-            error!("{}", err);
-            return Err(DatabaseError::DataCorruptionAttempt);
-        }
+    // pub fn insert(
+    //     connection: &mut PgConnection,
+    //     role_groups: &Vec<RoleGroup>,
+    // ) -> Result<Vec<RoleGroup>, DatabaseError> {
+    //     let untouchables: Vec<RoleGroupForm> = RoleGroupForm::get_predefined();
+    //     if let Err(err) =
+    //         is_data_secure::<RoleGroupForm, RoleGroup, RoleGroupConfig>(&untouchables, role_groups)
+    //     {
+    //         error!("{}", err);
+    //         return Err(DatabaseError::DataCorruptionAttempt);
+    //     }
 
-        match diesel::insert_into(role_groups::table)
-            .values(role_groups)
-            .get_results(connection)
-        {
-            Ok(results) => Ok(results),
-            Err(err) => {
-                error!("{}", err);
-                return Err(DatabaseError::DataCreateFailed);
-            }
-        }
-    }
+    //     match diesel::insert_into(role_groups::table)
+    //         .values(role_groups)
+    //         .get_results(connection)
+    //     {
+    //         Ok(results) => Ok(results),
+    //         Err(err) => {
+    //             error!("{}", err);
+    //             return Err(DatabaseError::DataCreateFailed);
+    //         }
+    //     }
+    // }
 
     // pub fn update(
     //     connection: &mut PgConnection,
@@ -124,20 +153,15 @@ impl RoleGroup {
         };
 
         if any_rows.len() == 0 {
-            let predefined = RoleGroup::get_predefined();
-            seed_file_check(seed_file_path, predefined);
+            seed_file_check::<RoleGroupForm, RoleGroupForm, RoleGroupConfig>(
+                seed_file_path,
+                RoleGroupForm::get_predefined(),
+            );
             // RoleGroup::insert(connection, &any_rows);
         } else {
             //RoleGroup::update(connection, &any_rows);
         }
 
         Ok(())
-    }
-
-    fn get_predefined() -> Vec<RoleGroupForm> {
-        vec![RoleGroupForm {
-            name: SYSTEM_ROLE_GROUP_NAME.to_string(),
-            config: Some(json!({"level":10000})),
-        }]
     }
 }
