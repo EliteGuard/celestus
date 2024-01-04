16e8a987a064d6294f349af899c19e3fb0e18bd6
Iliya Draganov <idraganov90@gmail.com>
2024-01-04 20:47:19 +0200

    fixed settings cache

--- src/lib/cache/settings.rs
+++ src/lib/cache/settings.rs
@@ -1,3 +1,4 @@
+use log::info;
 use lru::LruCache;
 
 pub type SettingsCache<'a> = LruCache<&'a str, &'a str>;
@@ -6,8 +7,9 @@ pub fn load_settings<'a>(cache: &mut SettingsCache) {
     import_settings(vec![("asd", "qwe")], cache);
 }
 
-fn import_settings<'a>(settings: Vec<(&'a str, &'a str)>, cache: &'a mut SettingsCache) {
+fn import_settings<'a>(settings: Vec<(&'a str, &'a str)>, cache: &mut SettingsCache<'a>) {
     for setting in settings.iter() {
-        // cache.push(setting.0, setting.1);
+        cache.push(setting.to_owned().0, setting.to_owned().1);
     }
+    info!("{:?}", cache.get(settings[0].0))
 }
